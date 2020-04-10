c3c30bfc744a4493140795d9df6fadeffb3255d0
Charls Dzull <charls.dzul.11@gmail.com>
2020-04-09 20:45:43 -0700

    Preparando Pulpox

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/pulpox_ads";
+  process.env.MONGOLAB_URI || "mongodb://localhost:27017/pulpox_ads";
 
 require("@babel/register")({
   presets: ["@babel/preset-react"],
